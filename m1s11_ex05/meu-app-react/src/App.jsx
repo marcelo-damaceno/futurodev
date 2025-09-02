5662d0031b98a52019d1428312f63994a34430c7
JV01\marcelo.damaceno <marcelo.damaceno@totvs.com.br>
2025-09-01 22:00:41 -0300

    arquivos iniciais

---
+++ m1s11_ex05/meu-app-react/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import RegistrationForm from "./components/RegistrationForm";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Cadastro de Usuário</h1>
+      <RegistrationForm />
+    </div>
+  );
+}
+
+export default App;
